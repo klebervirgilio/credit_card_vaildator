21b6a2074c2ade617afdc93ebe35cb7663566813
klebervirgilio <klebervirgilio@gmail.com>
2013-09-12 15:54:26 -0300

    Link for code base

--- lib/credit_card_formatting.rb
+++ lib/credit_card_formatting.rb
@@ -1,4 +1,5 @@
 module CreditCardFormatting
+  # https://github.com/Shopify/active_merchant/blob/master/lib/active_merchant/billing/credit_card_methods.rb#L5
   CARD_PATTERNS = {
     'visa'       => /\A4\d{12}(\d{3})?\z/,
     'mastercard' => /\A(5[1-5]\d{4}|677189)\d{10}\z/,
